152be37ed17eb75489a7cda070a98399fa6ae88f
Shehanka <hcsperera@outlook.com>
2018-12-17 14:23:14 +0530

    Login component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './login/email/email.component';
+import { SignupComponent } from './login/signup/signup.component';
+import { MembersComponent } from './login/members/members.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCD9e6vDbCfh9LeM6Q86lwqUgFJz7XQ4r4',
+  authDomain: 'angular7-crud-7cb22.firebaseapp.com',
+  databaseURL: 'https://angular7-crud-7cb22.firebaseio.com',
+  projectId: 'angular7-crud-7cb22',
+  storageBucket: 'angular7-crud-7cb22.appspot.com',
+  messagingSenderId: '58507482477'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
