61ecfd2ca4b953f8ab70f503edeb8f44d20b0f9d
Shehanka <hcsperera@outlook.com>
2018-12-21 21:48:25 +0530

    data table successfully loaded

--- src/app/developers/developer-list/developer-list.component.ts
+++ src/app/developers/developer-list/developer-list.component.ts
@@ -12,13 +12,6 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   {name: 'Hydrogen', weight: 'developer', symbol: 'H'},
 /*  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},*/
 ];
 
@@ -30,7 +23,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class DeveloperListComponent implements OnInit {
   list: Developer[];
   displayedColumns: string[] = ['name', 'weight', 'symbol'];
-  dataSource = this.list;
+  dataSource = ELEMENT_DATA;
 
   constructor(
       private service: DeveloperService,
@@ -45,6 +38,16 @@ export class DeveloperListComponent implements OnInit {
           ...item.payload.doc.data()} as Developer;
       });
     });
+  }
+
+  onEdit(dev: Developer) {
+    this.service.formData = Object.assign({}, dev);
+  }
+
+  onDelete(id: string) {
+    if (confirm('Are you sure to delete this record?')) {
+      this.firestore.doc('developers/' + id).delete();
+    }
   }
 
 }
